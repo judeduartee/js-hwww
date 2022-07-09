2a05ec63a8e699339bb899adca21ed600bdac0a2
bb <ddsd@mail.ru>
2022-07-09 10:11:48 +0500

    project

---
+++ script.js
@@ -0,0 +1,43 @@
+
+
+ var text = prompt('Введите своё имя');
+ console.log(text);
+
+var num = +prompt( 'Введите свой возраст');
+ console.log(num);
+ 
+
+
+  var total = prompt('10 + 15 = ?');
+
+  alert('Ваш ответ: ' + total  +  ' Правильный ответ: ' +  (10 + 15) );
+  console.log('10 + 15 = 25 ' + 'Ваш ответ: ' + total);
+
+
+
+ var minus = prompt('10 - 1 = ?');
+
+  alert('Ваш ответ: ' + minus  +  ' Правильный ответ: ' +  (10 - 1) );
+  console.log('10 - 1 = 9 ' + 'Ваш ответ: ' + minus);
+
+
+
+  var multiplication = prompt('10 * 1 = ?');
+
+ alert('Ваш ответ: ' + multiplication  +  ' Правильный ответ: ' +  10 * 1 );
+  console.log('10 * 1 = 10 ' + 'Ваш ответ: ' + multiplication);
+
+
+  var division = prompt('10 / 1 = ?');
+
+  alert('Ваш ответ: ' + division  +  ' Правильный ответ: ' +  10 / 1 );
+    console.log('10 / 1 = 10 ' + 'Ваш ответ: ' + division);
+
+
+  var rest = prompt('10 % 3 = ?');
+
+  alert('Ваш ответ: ' + rest +  ' Правильный ответ: ' +  10 % 3 );
+ console.log('10 % 3 = 1 ' + 'Ваш ответ: ' + rest);
+
+
+
