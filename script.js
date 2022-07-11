651eb425b87860956332c1f7d156b57e28935906
j <bbayushka@mail.ru>
2022-07-11 05:52:03 +0500

    project

--- script.js
+++ script.js
@@ -41,3 +41,14 @@ var num = +prompt( 'Введите свой возраст');
 
 
 
+ var X = +prompt( 'Введите значение X');
+ console.log(X);
+
+ var Y = +prompt( 'Введите значение Y');
+ console.log(Y);
+
+ var Z = +prompt( 'Введите значение Z');
+ console.log(Z);
+
+ alert( (X + Y + Z)/3 )
+
